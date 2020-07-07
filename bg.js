6d99464a66fe0b6f052c648d2abea77735387fb2
Devrim Sahin <kubuzetto@deepthought42.internet.milsoft.com.tr>
2020-07-07 18:58:47 +0300

    Waterfox compatibility

--- bg.js
+++ bg.js
@@ -1,19 +1,32 @@
 "use strict";
 
+// trial-and-error to keep Waterfox happy
+var updCtx = function(upd) {
+	try {
+		browser.contextMenus.update ("behind_ctxmenu", upd);
+	} catch (e) {
+		delete upd['visible'];
+		browser.contextMenus.update ("behind_ctxmenu", upd);
+	}
+};
+
 browser.contextMenus.create ({ "id": "behind_ctxmenu",
-	"title": browser.i18n.getMessage("menuButtonTextDefault"), "visible": false,
+	"title": browser.i18n.getMessage("menuButtonTextDefault"),
 	"contexts": [ "image", "link", "page", "audio", "video", "frame" ],
 	"onclick": function (x, t) {
-		browser.contextMenus.update ("behind_ctxmenu", { "visible": false });
+		updCtx({"visible": false});
 		let fn = function(opt) {
 			browser.tabs.create({ url: "/inline.html", active: opt && opt.bgCbox !== "f", openerTabId: t.id});
 		};
 		browser.storage.local.get('bgCbox').then(fn, () => fn({bgCbox: "t"}));
 	}});
+updCtx({"visible": false});
 
 // try to add an icon to the menu item: this is not currently supported in chrome and might fail
-const noop = function (a) {};
-browser.contextMenus.update ("behind_ctxmenu", {"icons": {"16": "icon.svg"}}).then (noop, noop);
+try {
+	const noop = function (a) {};
+	browser.contextMenus.update ("behind_ctxmenu", {"icons": {"16": "icon.svg"}}).then (noop, noop);
+} catch(e) {}
 
 browser.runtime.onMessage.addListener (function (fn, x) {
 	if (fn && fn.nm == "setClickedElements") {
@@ -27,7 +40,7 @@ browser.runtime.onMessage.addListener (function (fn, x) {
 			upd.title = browser.i18n.getMessage ("menuButtonTextWithNum", [fn.ce]);
 		else
 			upd.visible = false;
-		browser.contextMenus.update ("behind_ctxmenu", upd);
+		updCtx(upd);
 		// no refresh function in chrome
 		if (typeof browser.contextMenus.refresh === "function")
 			browser.contextMenus.refresh ();
