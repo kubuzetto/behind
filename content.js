70fa0d750b5123651209c7ee6d8d68a5f1397f5f
Devrim Şahin <devrim.sahin@picussecurity.com>
2022-03-13 23:00:52 +0300

    Fix srcset split logic for URLs with commas in them
    Signed-off-by: Devrim Şahin <devrim.sahin@picussecurity.com>

--- content.js
+++ content.js
@@ -38,8 +38,8 @@ var elemsFromPoint = function (x, y, add) {
 				add (n.currentSrc, false, nn);
 				add (n.poster, false, "IMG");
 				if (n.srcset) n.srcset.replace
-					(/\s+[0-9]+(\.[0-9]+)?[wx]/g, "")
-					.split (/,/).forEach (function (x) {add (x, false, nn);});
+					(/\s+[0-9]+(\.[0-9]+)?[wx],\n?/g, "\n")
+					.split (/\n/).forEach (function (x) {add (x, false, nn);});
 			}
 			var nrm = getComputedStyle (n);
 			var bef = getComputedStyle (n, "::before");
